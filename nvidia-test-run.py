11db54c06244e563b3be215b9865d8c9f5b76860
Apurva Shukla <95739011+apurvashukla1999@users.noreply.github.com>
2023-09-01 19:33:34 +0530

    TO-DO: fix redundant code

--- nvidia-test-run.py
+++ nvidia-test-run.py
@@ -1,72 +1,159 @@
-from selenium import webdriver
-from selenium.webdriver.support.ui import Select
-from selenium.webdriver.common.by import By
-# from selenium.webdriver.chrome.service import Service as ChromiumService
-# from webdriver_manager.chrome import ChromeDriverManager
-# from webdriver_manager.core.utils import ChromeType
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-import time
+# import time
 
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+# from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.wait import WebDriverWait
+
+# from webdriver_manager.chrome import ChromeDriverManager
+# from webdriver_manager.core.os_manager import ChromeType
 
 chrome_options = Options()
-chrome_options.add_experimental_option("detach", True)
+chrome_options.add_argument("--headless")
+#  chrome_options.add_experimental_option("detach", True)
+driver = webdriver.Chrome(options=chrome_options)
 
-driver = webdriver.Chrome()
-# driver = webdriver.Chrome(options=chrome_options, service=ChromiumService(ChromeDriverManager(
-# chrome_type=ChromeType.CHROMIUM).install()))
+# driver = webdriver.Chrome(
+#     options=chrome_options,
+#     service=Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()))
 driver.get("https://www.nvidia.com/Download/index.aspx?")
-driver.implicitly_wait(10)
+waitTimer = WebDriverWait(webdriver, 10)  # .until(ec._element_if_visible("Option"))
+# function implementation
 
+print("waiting for the website to load completely.")
 print("Hello, python user input script here! do your stuff")
 print("This is going to print out the product series dropdown list")
 productType = driver.find_element(By.ID, "selProductSeriesType")
 drop = Select(productType)
-element = WebDriverWait(webdriver, 10).until((EC.element_to_be_selected(drop.options[0])))
-drop.select_by_value("1")
+element = WebDriverWait(webdriver, 10).until((ec.element_to_be_selected(drop.options[0])))
 options = drop.options
-for index in range(0, len(options)):
-    print(options[index].text)
-productTypeValue = input("Enter values:")
-if productTypeValue == "GeForce":
-    drop.select_by_value("1")
-else:
-    exit()
 
+dict_options = {}
+
+for index in range(0, len(options)):
+    item = options[index]
+    dict_options[item.get_attribute('value')] = item.text
+
+print(dict_options)
+
+while True:
+    productTypeValue = input("Enter the number corresponding to your product series:")
+    if productTypeValue in dict_options:
+        break
+drop.select_by_value(productTypeValue)
 
 # product Series Type
-productType = driver.find_element(By.ID, "selProductSeriesType")
-drop = Select(productType)
-element = WebDriverWait(webdriver, 10).until((EC.element_to_be_selected(drop.options[0])))
-drop.select_by_value("1")
-options = drop.options
-for index in range(1, len(options)-1):
-    print(options[index].text)
+# productType = driver.find_element(By.ID, "selProductSeriesType")
+# drop = Select(productType)
+# element = WebDriverWait(webdriver, 10).until((EC.element_to_be_selected(drop.options[0])))
+# drop.select_by_value("1")
+# options = drop.options
+# for index in range(0, len(options)):
+#     print(options[index].text)
 # product Series
 productSeries = driver.find_element(By.ID, "selProductSeries")
 drop = Select(productSeries)
-drop.select_by_value("113")
+element = WebDriverWait(webdriver, 10).until((ec.element_to_be_selected(drop.options[0])))
+options = drop.options
+
+dict_options = {}
+
+for index in range(0, len(options)):
+    item = options[index]
+    dict_options[item.get_attribute('value')] = item.text
+
+print(dict_options)
+
+while True:
+    productSeriesValue = input("Enter the number corresponding to your product series:")
+    if productSeriesValue in dict_options:
+        break
+drop.select_by_value(productSeriesValue)
 # Product Family
 
 productFamily = driver.find_element(By.ID, "selProductFamily")
 drop = Select(productFamily)
-drop.select_by_value("893")
+element = WebDriverWait(webdriver, 10).until((ec.element_to_be_selected(drop.options[0])))
+options = drop.options
+
+dict_options = {}
+
+for index in range(0, len(options)):
+    item = options[index]
+    dict_options[item.get_attribute('value')] = item.text
+
+print(dict_options)
+
+while True:
+    productFamilyValue = input("Enter the number corresponding to your product:")
+    if productFamilyValue in dict_options:
+        break
+
+drop.select_by_value(productFamilyValue)
 
 # Operating System
 operatingSystem = driver.find_element(By.ID, "selOperatingSystem")
 drop = Select(operatingSystem)
-drop.select_by_value("135")
+element = WebDriverWait(webdriver, 10).until((ec.element_to_be_selected(drop.options[0])))
+options = drop.options
+
+dict_options = {}
+
+for index in range(0, len(options)):
+    item = options[index]
+    dict_options[item.get_attribute('value')] = item.text
+
+print(dict_options)
+
+while True:
+    productOSValue = input("Enter the number corresponding to your product:")
+    if productOSValue in dict_options:
+        break
+
+drop.select_by_value(productOSValue)
 
 # Windows Driver Type
 driverType = driver.find_element(By.ID, "ddlDownloadTypeCrdGrd")
 drop = Select(driverType)
-drop.select_by_value("1")
+element = WebDriverWait(webdriver, 10).until((ec.element_to_be_selected(drop.options[0])))
+options = drop.options
+
+dict_options = {}
+
+for index in range(0, len(options)):
+    item = options[index]
+    dict_options[item.get_attribute('value')] = item.text
+
+print(dict_options)
+
+while True:
+    productDriverValue = input("Enter the number corresponding to your product:")
+    if productDriverValue in dict_options:
+        break
+drop.select_by_value(productDriverValue)
 
 # Select Language
 ddLanguage = driver.find_element(By.ID, "ddlLanguage")
 drop = Select(ddLanguage)
-drop.select_by_value("1")
+element = WebDriverWait(webdriver, 10).until((ec.element_to_be_selected(drop.options[0])))
+options = drop.options
+
+dict_options = {}
+
+for index in range(0, len(options)):
+    item = options[index]
+    dict_options[item.get_attribute('value')] = item.text
+
+print(dict_options)
+
+while True:
+    productLangValue = input("Enter the number corresponding to your product:")
+    if productLangValue in dict_options:
+        break
+drop.select_by_value(productLangValue)
 # for use with NVIDIA Advanced Driver Search
 # Recommended Driver
 # ddWHQL = driver.find_element(By.ID, "ddWHQL")
@@ -75,11 +162,8 @@ drop.select_by_value("1")
 # only selects certified drivers
 
 # click search button
-
 searchButton = driver.find_element(By.XPATH, ".//a[contains(@href, 'GetDriver')]")
 searchButton.click()
-
-time.sleep(5)
 
 # click download button 
 downloadButton = driver.find_element(By.ID, "lnkDwnldBtn")
@@ -93,16 +177,20 @@ hrefDownload.click()
 driver.implicitly_wait(10)
 
 
-def every_downloads_chrome(driver):
-    if not driver.current_url.startswith("chrome://downloads"):
-        driver.get("chrome://downloads/")
-    return driver.execute_script("""
+def every_downloads_chrome(cdriver):
+    driver.implicitly_wait(5)
+    if not cdriver.current_url.startswith("chrome://downloads"):
+        cdriver.get("chrome://downloads/")
+    return cdriver.execute_script("""
         return document.querySelector('downloads-manager')
-        .shadowRoot.querySelector('#downloadsList')
-        .items.filter(e => e.state === 'COMPLETE')
-        .map(e => e.filePath || e.file_path || e.fileUrl || e.file_url);
+            .shadowRoot.querySelector('#downloadsList')
+            .items.filter(e => e.state === 'COMPLETE')
+            .map(e => e.filePath || e.file_path || e.fileUrl || e.file_url);
         """)
 
+
+print("The downloaded location will be updated below: ")
+every_downloads_chrome(driver)
 
 paths = WebDriverWait(driver, 120, 1).until(every_downloads_chrome)
 print(paths)
